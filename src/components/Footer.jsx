7a4941290e77e8f2cdf84a2d0acdbbc9ae1bed0c
Ghaeth Mrad <mradgaeth40@gmail.com>
2024-12-15 05:37:08 +0000

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="py-[30px] bg-[#f6f6f6] flex justify-center items-center text-[#888]">
-        <p className="text-[14px] text-center">© 2024 Issaaf Kattan React Land Page Template. Design by Ghaeth Mrad</p>
-    </footer>
-  )
-}
-
-export default Footer
